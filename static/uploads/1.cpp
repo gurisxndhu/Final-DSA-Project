7dce56c154d455bd1e934ee779491dc5f66cd3a6
Gurinder Singh Sandhu <133472518+gurisxndhu@users.noreply.github.com>
2023-08-13 14:20:38 -0400

    Add files via upload

---
+++ static/uploads/1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int main()
+	
+	int rows;
+	std::cout<<"Enter number of rows";
+	std::cin>>rows;
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			std::cout<<j;	
+		}
+		
+		std::cout<<"\n";
+	}
+	
+	
+	
+	
+
